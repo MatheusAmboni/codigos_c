7d3a0198cc7ac9f018b768cacbe1cb934470f84f
MatheusAmboni <mateussamboni@gmail.com>
2020-12-07 15:56:29 -0300

    Now returning correct values

--- final_activity/main.c
+++ final_activity/main.c
@@ -4,9 +4,7 @@
 
 int c;
 int b;
-int hora;
-int minuto;
-int diaSemana;
+char horario[5] = "";
 
 int main() {
    printf("+-----------------------+\n"
@@ -14,23 +12,24 @@ int main() {
         "|       barbearia       |\n"
         "|  Chacarosque e Amboni |\n"
         "+-----------------------+\n");
-   printf("Por favor, solicite como quer o seu corte: \n");
-   printf("\n");
+   printf("Por favor, solicite como quer o seu corte: \n\n");
    printf("Apenas Cabelo:     digite (1) \n");
    printf("Apenas Barba:      digite (2) \n");
-   printf("Barba e cabelo:    digite (3) \n");
-   printf("\n");
+   printf("Barba e cabelo:    digite (3) \n\n");
    scanf("%d", &c);
    printf("\n");
-   printf("Agora por favor, solicite com qual barbeiro queres o servico: \n");
-   printf("\n");
+   printf("Agora por favor, solicite com qual barbeiro queres o servico: \n\n");
    printf("Matheus:     digite (1) \n");
    printf("Antonio:     digite (2) \n");
-   printf("Carlos:      digite (3) \n");
-   printf("\n");
+   printf("Carlos:      digite (3) \n\n");
    scanf("%d", &b);
    printf("\n");
    printf("Agora por favor, solicite o horario preferido: \n");
+   printf("Formato: hh:mm \n\n");
+   scanf("%s", horario);
+   printf("\n");
+   printf("Horario marcado com o barbeiro %d, sendo %d e no horario %s. \n", c, b, horario);
+   printf("Aguardamos sua presenca.");
    
    return 0;
 }
