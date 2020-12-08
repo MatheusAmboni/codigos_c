c89c0932510dfdc7ec047fad33fbcc91f2eb0a16
MatheusAmboni <mateussamboni@gmail.com>
2020-12-08 19:27:06 -0300

    final_changes(?)

--- final_activity/main.c
+++ final_activity/main.c
@@ -10,55 +10,32 @@ char *barbeiro = "";
 char *corte = "";
 char horario[5] = "";
 
-int switch1(int , char *);
-void switch2(int , char *);
-
 int main() {
    menu1();
    scanf("%d", &c);
-   v = switch1(c, corte);
-   menu2();
-   scanf("%d", &b);
-   switch2(b, barbeiro);
-   printf("Agora por favor, solicite o horario preferido: \n");
-   printf("Formato: hh:mm \n\n");
-   scanf("%s", horario);
-   printf("\n");
-   printf("Horario solicitado com o\
-   \b\bbarbeiro %s, o corte sendo %s as %s horas! \n", barbeiro, corte, horario );
-   printf("O valor fica: %d,00 R$\n", v);
-   printf("Entraremos em contato com voce em breve\n");
-   return 0;
-}
-
-int switch1(int c, char *corte) {
-   int valor;
-   
    switch (c)
    {
    case 1 :
       printf("Voce selecionou cabelo\n");
       corte = "apenas cabelo";
-      valor = 40;
+      v = 40;
       break;
    case 2 :
       printf("Voce selecionou Barba\n");
       corte = "apenas barba";
-      valor = 15;
+      v = 15;
       break;
    case 3 :
       printf("Voce selecionou barba e cabelo\n");
       corte = "barba e cabelo";
-      valor = 50;
+      v = 50;
       break;
    default:
       printf("Selecione um numero de 1 a 3\n");
-      valor = 0;
+      return 0;
    }
-   return valor;
-}
-
-void switch2 (int b, char *barbeiro) {
+   menu2();
+   scanf("%d", &b);
    switch (b)
    {
    case 1 :
@@ -79,6 +56,19 @@ void switch2 (int b, char *barbeiro) {
       break;
    default:
       printf("Selecione um numero de 1 a 4\n");
-      return;
+      return 0;
    }
+   printf("Agora por favor, solicite o horario preferido: \n");
+   printf("Formato: hh:mm \n\n");
+   scanf("%s", horario);
+   printf("\n");
+   printf("Horario solicitado com o\
+   \b\bbarbeiro %s, o corte sendo %s as %s horas! \n", barbeiro, corte, horario );
+   printf("O valor fica: %d,00 R$\n", v);
+   printf("Entraremos em contato com voce em breve\n");
+   return 0;
 }
+
+
+  
+
